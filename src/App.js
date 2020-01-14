930ccc494fc43ad83ed28b7604fd578a84c6de71
Aaron Andersen <anderseana@tutanota.com>
2020-01-13 19:36:47 -0600

    got most of the stuff working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import TodoList from "./components/TodoComponents/TodoList";
+// import TodoForm from "./components/TodoForm";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  constructor() {
+    super();
+    this.state = {
+      todo: [
+        {task: "Finish Lambda",
+         id: 0,
+         complete: false},
+        
+        {task: "Get A Job in Web Dev",
+         id: 1,
+         complete: false},
+         
+        {task: "Invest in Stock",
+         id: 2,
+         complete: false},
+
+        {task: "Buy A Home",
+         id: 3,
+         complete: false}
+      ],
+      todo: ""
+    }
+  }
+
+    addContent = event => {
+      event.prevenDefault();
+      const newItem = {task: this.state.todo, completed: false, id: Date.now()}
+      this.setState({
+        todostuff: [...this.state.todos, newItem],
+        todo: ""
+      })
+    }
+
+    handleContent = event => {
+      this.setState({[event.target.name]:event.target.value})
+    }
+
+    handleCompletedObjects = id => {
+      const finished = this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+          return todo
+        }
+        else { return todo}
+      })
+      this.setState({finished})
+    }
+    
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="todolist">
+        <h2>The To Do app.</h2>
+        <TodoList todo={this.state.todos} handleCompletedObjects={this.handleCompletedObjects}/>
+        {/* <TodoForm
+          value={this.state.todo}
+          addContent={this.addContent}
+          handleContent={this.handleContent}/> */}
       </div>
     );
   }
